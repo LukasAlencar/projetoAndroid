0279fb0fdffe03bc191cf156fb367f4b692ea6c1
Lucas Kauan <lucas.kauan@aluno.ifsp.edu.br>
2022-05-18 12:40:23 -0300

    Corrigindo menu e implementando JS

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function irParaContatos(){
+    var nome = document.querySelector('input#name')
+    window.location.href = 'index.html#contatos'
+    nome.focus()
+}
+
+function irParaCuriosidades(){
+    window.location.hre = 'index.html#curiosidades'
+}
